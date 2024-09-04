5d58dabebb95e36442dfbe54ab3951e5a680c884
imagion <algernoncowperwood@gmail.com>
2024-09-04 09:37:30 +0300

    added tree menu

---
+++ app/components/Tree.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import DiscordLogo from '@/public/discord.svg';
+
+export default function Tree() {
+  return (
+    <nav className='flex w-[72px] flex-col items-center overflow-hidden bg-[#1e1f22] pt-3'>
+      <div className='tree__list-item mb-2'>
+        <DiscordLogo width={30} height={30} />
+      </div>
+      <div className='mb-2'>
+        <div className='h-[2px] w-8 rounded-sm bg-slate-700'></div>
+      </div>
+      <div className='tree__list-item mb-2'>2</div>
+      <div className='tree__list-item mb-2'>3</div>
+      <div className='tree__list-item mb-2'>4</div>
+    </nav>
+  );
+}
+
+console.log('Hello');
