814a6b533eb18204706655f6d70b844dc6d0961e
imagion <algernoncowperwood@gmail.com>
2024-09-20 17:25:36 +0300

    added auth is ready action

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,34 @@
+'use client';
+
 import Tree from '@/components/Tree';
 import Sidebar from '@/components/Sidebar';
 import ChatForm from '@/components/ChatForm';
+import { useAuthContext } from '@/hooks/useAuthContext';
 
 export default function App() {
+  const { state } = useAuthContext();
+
   return (
-    <div className='flex flex-auto'>
-      <Tree />
-      <Sidebar />
-      <div className='relative flex flex-auto flex-col overflow-hidden bg-neutral-600'>
-        <section className='flex min-h-12 w-full flex-initial cursor-default items-center p-2 shadow-md'>
-          <h1>Channel Name</h1>
-        </section>
-        <main className='flex flex-auto flex-col'>
-          <ul className='scroller inset-0 flex flex-auto flex-col justify-end overflow-x-hidden overflow-y-scroll'>
-            <li>test</li>
-            <li>hi</li>
-            <li>hello</li>
-          </ul>
-          <ChatForm />
-        </main>
-      </div>
-    </div>
+    <>
+      {state.authIsReady && (
+        <div className='flex flex-auto'>
+          <Tree />
+          <Sidebar />
+          <div className='relative flex flex-auto flex-col overflow-hidden bg-neutral-600'>
+            <section className='flex min-h-12 w-full flex-initial cursor-default items-center p-2 shadow-md'>
+              <h1>Channel Name</h1>
+            </section>
+            <main className='flex flex-auto flex-col'>
+              <ul className='scroller inset-0 flex flex-auto flex-col justify-end overflow-x-hidden overflow-y-scroll'>
+                <li>test</li>
+                <li>hi</li>
+                <li>hello</li>
+              </ul>
+              <ChatForm />
+            </main>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
