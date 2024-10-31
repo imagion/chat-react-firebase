8be0eaf6e1d6377f9cffe729b8b58d79eb46c267
imagion <algernoncowperwood@gmail.com>
2024-10-31 13:19:08 +0300

    fixing signup users

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
+        pathname: '/v0/b/discord-clone-110c0.appspot.com/o/**',
       },
     ],
   },
